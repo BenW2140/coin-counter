62a852a8c10ad836361c769d59ab11e545880b89
Ben White <bwhite2140@outlook.com>
2020-11-09 14:21:03 -0800

    add working recursive function for counting coins

--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,3 +1,33 @@
-export const countCoins = (amount) => {
-  return 0;
+export const countCoins = (amount, coins) => {
+  if (isNaN(amount)) {
+    return;
+  }
+  if (amount === 0) {
+    return [];
+  } else {
+    if (amount >= coins[0]) {
+      const left = (amount - coins[0]);
+      return [coins[0]].concat(countCoins(left, coins));
+    } else {
+      coins.shift();
+      return countCoins(amount, coins);
+    }
+  }
+}
+
+const typeOfCoin = (amount) => {
+  return (amountOfCoins) => {
+    return amount * amountOfCoins;
+  }
+}
+
+const factorial = (number) => {
+  if (isNaN(number)) {
+    return;
+  }
+  if (number === 0) {
+    return 1;
+  } else {
+    return factorial(number - 1) * number;
+  }
 }
